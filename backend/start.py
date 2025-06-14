377cb5c781cd69ab65926e343b84b6b43b102083
Iyyappan R <iyyappansurya50@gmail.com>
2025-06-14 23:46:03 +0530

    bind port start.py for render

--- backend/start.py
+++ backend/start.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # fallback for local dev
+    port = int(os.environ.get("PORT", 10000))  # fallback
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
